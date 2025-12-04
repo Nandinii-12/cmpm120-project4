0aec524a64bb014d61d9bac6ae5ddd1d15e2f40e
wopungpu-wq <wopungpu@ucsc.edu>
2025-12-03 18:15:36 -0800

    unknown changes

--- CMPM120-Skeleton-main/src/main.js
+++ CMPM120-Skeleton-main/src/main.js
@@ -1,19 +1,28 @@
-import { Start } from './scenes/Start.js';
+import { Level1 } from "./scenes/Level1.js";
+import { Level2 } from "./scenes/Level2.js";
 
 const config = {
     type: Phaser.AUTO,
     title: 'CMPM 120 Project Skeleton',
     description: '',
     parent: 'game-container',
-    width: 1270,
+    width: 1280,
     height: 720,
     backgroundColor: '#000000',
-    pixelArt: false,
-    scene: [
-        Start
-    ],
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                x: 0,
+                y: 0
+            },
+            debug: true
+        }
+    },
+    scene: [/*GameMenu,*/ Level1, Level2 /*, Win, GameOver */],
     scale: {
-        mode: Phaser.Scale.FIT,
+        mode: Phaser.Scale.ENVELOP,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
 }
---
+++ CMPM120-Skeleton-main/src/scenes/Level1.js
@@ -0,0 +1,9 @@
+import { BaseLevel } from "./BaseLevel.js";
+
+export class Level1 extends BaseLevel {
+    constructor() {
+        super("Level1", "lvl1");
+    }
+}
+
+//Untested
