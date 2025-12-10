a4418bc226b66206ab5c3640dfdf31c4f559ef45
Nandinii-12 <naiyer@ucsc.edu>
2025-12-10 03:44:54 -0800

    added keys text in top right corner of screen

--- CMPM120-Skeleton-main/src/scenes/BaseLevel.js
+++ CMPM120-Skeleton-main/src/scenes/BaseLevel.js
@@ -30,12 +30,31 @@ export class BaseLevel extends Phaser.Scene {
         this.animFrameDuration = 240; // How fast tiles animates
         this.last_time = 0;
         this.dialogueActive = false;
+        this.keysCollected = 0;
 
         this.makeTilemap();
         this.setKeyboards();
         this.setUpPlayer();
         this.setCamera(3);
         this.setUpNPCs();
+
+        this.keyText = this.add.text(0, 0, "Keys collected: " + this.keysCollected, {
+            fontFamily: 'Arial',
+            fontSize: '9px',
+            color: '#ffffff',
+            backgroundColor: '#000000',
+            padding: { x: 2, y: 2 },
+            align: 'center',
+        });
+
+        // Center on screen
+        this.keyText.setPosition(
+            (this.cameras.main.width / 2 - this.keyText.width / 2)+150,
+            (this.cameras.main.height / 2 - this.keyText.height / 2)-55
+        );
+
+        // Fix it to camera
+        this.keyText.setScrollFactor(0);
     }
 
     update(time, delta) {
@@ -136,6 +155,8 @@ export class BaseLevel extends Phaser.Scene {
                         this.dialogueActive = false;
                         if(this.bucketDelivered)
                         {
+                            this.keysCollected++;
+                            this.keyText.setText("Keys collected: " + this.keysCollected);
                             this.girl.destroy();
                         }
                     });
